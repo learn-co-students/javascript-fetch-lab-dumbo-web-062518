77d2fa7bc280f1a6a589369f843da6b00607ae17
Jane Costa <yberdysh@gmail.com>
2018-08-12 16:21:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const token = '';
+ 
+const postData = {
+  body: 'Great stuff'
+};
+ 
+fetch('https://api.github.com/repos/:your_ghname/:your_repo/commits/:sha/comments', {
+  method: 'POST',
+  body: JSON.stringify(postData),
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => console.log(res));
+
+
 function getIssues() {
 }
 
